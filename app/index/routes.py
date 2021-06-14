022c64435a94bc7f9c512f0d7166c5370f3527d7
Damian <m.d.ontivero@gmail.com>
2021-06-14 22:08:17 +0200

    Environments corrections

--- app/index/routes.py
+++ app/index/routes.py
@@ -4,7 +4,10 @@ from . import index_bp
 
 from flask_login import login_required
 
+from app.environments.models import Environment
+
 @index_bp.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    return render_template('index.html')
+    environments = Environment.get_all()
+    return render_template('index.html', environments=environments)
