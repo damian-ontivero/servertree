1e96860c69c2fd7ccb53dc7fcfe1bd01c823a6d5
Damian <m.d.ontivero@gmail.com>
2021-06-20 02:10:55 +0200

    commit

--- app/index/routes.py
+++ app/index/routes.py
@@ -5,9 +5,11 @@ from . import index_bp
 from flask_login import login_required
 
 from app.environments.models import Environment
+from app.auth.forms import UserForm
 
 @index_bp.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    user_form = UserForm()
     environments = Environment.get_all()
-    return render_template('index.html', environments=environments)
+    return render_template('index.html', user_form=user_form, environments=environments)
