2a26afbfec9807a6e270ce530f28a1c4047a9b66
Damian Ontivero <m.d.ontivero@gmail.com>
2021-11-28 03:06:29 +0100

    Refactor - First approach to DDD

--- src/service/service_abstract.py
+++ src/service/service_abstract.py
@@ -4,15 +4,6 @@ from typing import List
 
 from repository.repository import Repository
 
-from model.auth.user import UserModel  # noqa: F401
-from model.auth.role import RoleModel  # noqa: F401
-from model.connection_type.connection_type import ConnectionTypeModel  # noqa: F401
-from model.environment.environment import EnvironmentModel  # noqa: F401
-from model.operating_system.operating_system import OperatingSystemModel  # noqa: F401
-from model.server.server import ServerModel  # noqa: F401
-from model.server.access import AccessModel  # noqa: F401
-from model.server.service import ServiceModel  # noqa: F401
-
 
 class ServiceAbstract:
     """Doc."""
